a7995f3d2f62a333193b0e37070b22bbb7fa6519
KozlovskyS <skozmail@gmail.com>
2025-05-02 12:27:34 +0300

    add HW-8 task 1

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,35 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+    let cache = [];
+
+    function wrapper(...args) {
+        const hash = md5(args);
+        let objectInCache = cache.find((obj) => hash in obj);
+
+        if (objectInCache) {
+            console.log("Из кеша: " + objectInCache[hash]);
+            return "Из кеша: " + objectInCache[hash];
+        }
+
+        let result = func(...args);
+        cache.push({[hash]: result});
+
+        if (cache.length > 5) {
+            cache.shift();
+        }
+
+        console.log("Вычисляем: " + result);
+        return "Вычисляем: " + result;
+    }
+
+    return wrapper;
 }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+
+    function wrapper(...args) {
+
+    }
+    return wrapper;
 }
